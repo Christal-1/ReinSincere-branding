faf2464bd07b20584d5b8ef3fbd9820d9707cc80
Christal Haines <christal.haines@nagarro.com>
2026-03-04 11:09:30 +0200

    Update project

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ReinSincere-branding/',  // ← Add this line
+  base: '/ReinSincere-branding/', // ← ensures paths work on GitHub Pages
   plugins: [react()],
 })
